266b71897e7865215211cf2f9cf3176f591a8d96
huahuaTeam <1791042023@qq.com>
2018-08-20 08:51:50 +0800

    Number333111222

--- src/org/java/service/impl/SysUserServiceImpl.java
+++ src/org/java/service/impl/SysUserServiceImpl.java
@@ -2,9 +2,6 @@ package org.java.service.impl;
 
 import java.util.List;
 import java.util.Map;
-
-import org.java.entity.OaTeamRole;
-import org.java.entity.OaTeamWorker;
 import org.java.mapper.OaTeamWorkerCustomMapper;
 import org.java.service.SysUserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,21 +11,15 @@ import org.springframework.stereotype.Service;
 public class SysUserServiceImpl implements SysUserService {
 
 	@Autowired
-	private OaTeamWorkerCustomMapper oaTeamWorkerCustomMapper;
+	private OaTeamWorkerCustomMapper OaTeamWorkerCustomMapper;
 
 	@Override
-	public OaTeamWorker loginInfo(String username, String password){
-		OaTeamWorker worker = oaTeamWorkerCustomMapper.loginInfo(username,password);
-		System.out.println(worker+"=====================================");
-		if(worker!=null){
-			return worker;
-		}else{
+	public Map<String, Object> loginInfo(String username) {
+		List<Map<String, Object>> list = OaTeamWorkerCustomMapper.loginInfo(username);
+		if (list.isEmpty()) {
 			return null;
+		} else{
+			return list.get(0);
 		}
-	}
-
-	@Override
-	public List<Map<String,Object>> one(){
-		return oaTeamWorkerCustomMapper.one();
 	}
 }
